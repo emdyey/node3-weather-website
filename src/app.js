e4c70d3c0890ec0e1e03cab6c6ed277b8c4b9b58
Mary Joy Eraes <maryjoyeraes@gmail.com>
2021-08-26 14:06:35 +0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -566,7 +566,6 @@ app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });
 
-*/
 
 ///// Browser HTTP Requests with Fetch /////
 
@@ -674,3 +673,114 @@ app.get("*", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });
+
+*/
+
+///// Deploying Node.js to Heroku /////
+
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Mary Joy Eraes",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Mary Joy Eraes",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    message: "How can we help you?",
+    name: "Mary Joy Eraes",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Address must be provided",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecasrData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecasrData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: 404,
+    name: "Mary Joy Eraes",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: 404,
+    name: "Mary Joy Eraes",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is up on port' + port);
+});
+
